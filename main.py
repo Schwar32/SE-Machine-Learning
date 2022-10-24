f1407bd19c178ef79ee1e9828074fc06896bfb16
Schwar32 <schwar32@students.rowan.edu>
2022-10-24 03:48:28 -0400

    Began building out a machine learning framework

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-import soundfile as sf
-import numpy as np
-from scipy.fft import rfft, rfftfreq
-import matplotlib.pyplot as plt
-import matplotlib
+import os
+from matplotlib import pyplot as plt
+import tensorflow as tf
+import tensorflow_io as tfio
 
 
-def testing():
-    data, sample_rate = sf.read("./Data/Audio/acafly/XC6671.ogg")
-
-    plt.specgram(data, Fs=sample_rate)
-    plt.savefig("testing.png", transparent=True, dpi=100)
+def load_audio(filename):
+    # Load encoded wav file
+    file_contents = tf.io.read_file(filename)
+    # Decode wav (tensors by channels)
+    wav, sample_rate = tf.audio.decode_wav(file_contents, desired_channels=1)
+    # Removes trailing axis
+    wav = tf.squeeze(wav, axis=-1)
+    sample_rate = tf.cast(sample_rate, dtype=tf.int64)
+    # Resamples to standard 16000Hz
+    wav = tfio.audio.resample(wav, rate_in=sample_rate, rate_out=16000)
+    return wav
 
 
 if __name__ == "__main__":
-    testing()
+    wav = load_audio("Data/Audio/acafly/XC6671.ogg")
+    print(wav)
