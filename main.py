9e4a080811faed56adb414cfb360e446999644ca
Schwar32 <schwar32@students.rowan.edu>
2022-10-27 11:56:16 -0400

    Small update

--- main.py
+++ main.py
@@ -38,6 +38,15 @@ def load_data():
     return positives.concatenate(negatives)
 
 
+def setup_data(data):
+    data = data.map(preprocess)
+    data = data.cache()
+    data = data.shuffle(buffer_size=1024)
+    data = data.batch(2)
+    data = data.prefetch(1)
+    return data
+
+
 def create_model():
     model = Sequential()
     model.add(Conv2D(16, (3, 3), activation='relu', input_shape=(1496, 257, 1)))
@@ -82,24 +91,39 @@ def train_model(epoch_amt, save):
         model.save("model")
 
 
+def check_model(test):
+    successes = 0
+    fails = 0
+    for x in range(len(test)):
+        X_test, y_test = test.as_numpy_iterator().next()
+        yhat = model.predict(X_test)
+        yhat = [1 if prediction > 0.5 else 0 for prediction in yhat]
+        if yhat[0] == y_test.astype(int)[0]:
+            successes += 1
+        else:
+            fails += 1
+        if yhat[1] == y_test.astype(int)[1]:
+            successes += 1
+        else:
+            fails += 1
+
+    print(successes)
+    print(fails)
+
+
 if __name__ == "__main__":
     os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
     data = load_data()
+    data = setup_data(data)
 
-    #filepath, label = positives.shuffle(buffer_size=10000).as_numpy_iterator().next()
+    # filepath, label = positives.shuffle(buffer_size=10000).as_numpy_iterator().next()
 
-    #spectrogram, label = preprocess(filepath, label)
+    # spectrogram, label = preprocess(filepath, label)
 
-    #plt.figure(figsize=(30, 20))
-    #plt.imshow(tf.transpose(spectrogram)[0])
-    #plt.show()
-
-    data = data.map(preprocess)
-    data = data.cache()
-    data = data.shuffle(buffer_size=1024)
-    data = data.batch(2)
-    data = data.prefetch(1)
+    # plt.figure(figsize=(30, 20))
+    # plt.imshow(tf.transpose(spectrogram)[0])
+    # plt.show()
 
     training_size = int(len(data) * .80)        # 80% of data is for training
     testing_size = len(data) - training_size    # 20% of data is for validating
@@ -115,20 +139,5 @@ if __name__ == "__main__":
     # train_model(10, False)
 
     model = load_model()
-    successes = 0
-    fails = 0
-    for x in range(len(data)):
-        X_test, y_test = test.as_numpy_iterator().next()
-        yhat = model.predict(X_test)
-        yhat = [1 if prediction > 0.5 else 0 for prediction in yhat]
-        if yhat[0] == y_test.astype(int)[0]:
-            successes += 1
-        else:
-            fails += 1
-        if yhat[1] == y_test.astype(int)[1]:
-            successes += 1
-        else:
-            fails += 1
+    check_model(test)
 
-    print(successes)
-    print(fails)
