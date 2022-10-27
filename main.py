fd4a2ad0fa4f68764e151d0d64e21a01e8fdb387
Schwar32 <schwar32@students.rowan.edu>
2022-10-27 11:46:50 -0400

    Seperating blocks into functions

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import keras.models
 from matplotlib import pyplot as plt
 import tensorflow as tf
 import tensorflow_io as tfio
-import matplotlib
 import soundfile as sf
 
 from keras.models import Sequential
@@ -12,8 +11,8 @@ from keras.layers import Conv2D, Dense, Flatten
 
 
 def load_audio(file_name):
-    data, sample_rate = sf.read(file_name.numpy())
-    return data[::4]
+    audio_data, sample_rate = sf.read(file_name.numpy())
+    return audio_data[::4]
 
 
 def preprocess(file_path, label):
@@ -21,15 +20,13 @@ def preprocess(file_path, label):
     wav = wav[:96000]
     zero_padding = tf.zeros([96000] - tf.shape(wav), dtype=tf.float32)
     wav = tf.concat([zero_padding, wav], 0)
-    spectrogram = tf.signal.stft(wav, frame_length=320, frame_step=64)
+    spectrogram = tf.signal.stft(wav, frame_length=512, frame_step=256)
     spectrogram = tf.abs(spectrogram)
     spectrogram = tf.expand_dims(spectrogram, axis=2)
     return spectrogram, label
 
 
-if __name__ == "__main__":
-    os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
-
+def load_data():
     POS = "Data/Audio/acafly/"
     NEG = "Data/Audio/ameavo/"
 
@@ -38,31 +35,10 @@ if __name__ == "__main__":
 
     positives = tf.data.Dataset.zip((pos, tf.data.Dataset.from_tensor_slices(tf.ones(len(pos)))))
     negatives = tf.data.Dataset.zip((neg, tf.data.Dataset.from_tensor_slices(tf.zeros(len(neg)))))
-    data = positives.concatenate(negatives)
+    return positives.concatenate(negatives)
 
-    #filepath, label = positives.shuffle(buffer_size=10000).as_numpy_iterator().next()
 
-    #spectrogram, label = preprocess(filepath, label)
-
-    #plt.figure(figsize=(30, 20))
-    #plt.imshow(tf.transpose(spectrogram)[0])
-    #plt.show()
-
-    data = data.map(preprocess)
-    data = data.cache()
-    data = data.shuffle(buffer_size=1024)
-    data = data.batch(2)
-    data = data.prefetch(1)
-
-    train = data.take(50)
-    test = data.skip(50).take(25)
-    """
-
-    #samples, labels = train.as_numpy_iterator().next()
-    #print("-----------------------------------------------------")
-    #print(samples.shape)
-    #print("-----------------------------------------------------")
-
+def create_model():
     model = Sequential()
     model.add(Conv2D(16, (3, 3), activation='relu', input_shape=(1496, 257, 1)))
     model.add(Conv2D(16, (3, 3), activation='relu'))
@@ -71,10 +47,14 @@ if __name__ == "__main__":
     model.add(Dense(1, activation='sigmoid'))
 
     model.compile('Adam', loss='BinaryCrossentropy', metrics=[tf.keras.metrics.Recall(), tf.keras.metrics.Precision()])
-    model.summary()
+    return model
 
-    hist = model.fit(train, epochs=10, validation_data=test)
 
+def load_model():
+    return keras.models.load_model("model")
+
+
+def display_training_data(hist):
     plt.title('Loss')
     plt.plot(hist.history['loss'], 'r')
     plt.plot(hist.history['val_loss'], 'b')
@@ -90,28 +70,62 @@ if __name__ == "__main__":
     plt.plot(hist.history['val_recall'], 'b')
     plt.show()
 
-    for x in range(20):
-        X_test, y_test = test.as_numpy_iterator().next()
-        yhat = model.predict(X_test)
-        yhat = [1 if prediction > 0.5 else 0 for prediction in yhat]
-        print(yhat)
-        print(y_test.astype(int))
 
-    model.save("model")
-    """
+def train_model(epoch_amt, save):
+    model = create_model()
 
-    model = keras.models.load_model("model")
+    hist = model.fit(train, epochs=epoch_amt, validation_data=test)
+
+    display_training_data(hist)
+
+    if save:
+        model.save("model")
+
+
+if __name__ == "__main__":
+    os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+    data = load_data()
+
+    #filepath, label = positives.shuffle(buffer_size=10000).as_numpy_iterator().next()
+
+    #spectrogram, label = preprocess(filepath, label)
+
+    #plt.figure(figsize=(30, 20))
+    #plt.imshow(tf.transpose(spectrogram)[0])
+    #plt.show()
+
+    data = data.map(preprocess)
+    data = data.cache()
+    data = data.shuffle(buffer_size=1024)
+    data = data.batch(2)
+    data = data.prefetch(1)
+
+    training_size = int(len(data) * .80)        # 80% of data is for training
+    testing_size = len(data) - training_size    # 20% of data is for validating
+
+    train = data.take(training_size)
+    test = data.skip(training_size).take(testing_size)
+
+    # samples, labels = train.as_numpy_iterator().next()
+    # print("-----------------------------------------------------")
+    # print(samples.shape)
+    # print("-----------------------------------------------------")
+
+    # train_model(10, False)
+
+    model = load_model()
     successes = 0
     fails = 0
-    for x in range(25):
+    for x in range(len(data)):
         X_test, y_test = test.as_numpy_iterator().next()
         yhat = model.predict(X_test)
         yhat = [1 if prediction > 0.5 else 0 for prediction in yhat]
-        if(yhat[0] == y_test.astype(int)[0]):
+        if yhat[0] == y_test.astype(int)[0]:
             successes += 1
         else:
             fails += 1
-        if (yhat[1] == y_test.astype(int)[1]):
+        if yhat[1] == y_test.astype(int)[1]:
             successes += 1
         else:
             fails += 1
