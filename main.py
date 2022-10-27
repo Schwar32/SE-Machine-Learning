41e36729b18fe31ced9492c68fe2d99c64fa5c20
Schwar32 <schwar32@students.rowan.edu>
2022-10-27 13:26:21 -0400

    Touch up before class

--- main.py
+++ main.py
@@ -20,15 +20,15 @@ def preprocess(file_path, label):
     wav = wav[:96000]
     zero_padding = tf.zeros([96000] - tf.shape(wav), dtype=tf.float32)
     wav = tf.concat([zero_padding, wav], 0)
-    spectrogram = tf.signal.stft(wav, frame_length=512, frame_step=256)
+    spectrogram = tf.signal.stft(wav, frame_length=256, frame_step=128)
     spectrogram = tf.abs(spectrogram)
     spectrogram = tf.expand_dims(spectrogram, axis=2)
     return spectrogram, label
 
 
 def load_data():
-    POS = "Data/Audio/acafly/"
-    NEG = "Data/Audio/ameavo/"
+    POS = "Data/Audio/banswa/"
+    NEG = "Data/Audio/batpig1/"
 
     pos = tf.data.Dataset.list_files(POS+'*')
     neg = tf.data.Dataset.list_files(NEG+'*')
@@ -42,14 +42,14 @@ def setup_data(data):
     data = data.map(preprocess)
     data = data.cache()
     data = data.shuffle(buffer_size=1024)
-    data = data.batch(2)
-    data = data.prefetch(1)
+    data = data.batch(8)
+    data = data.prefetch(4)
     return data
 
 
 def create_model():
     model = Sequential()
-    model.add(Conv2D(16, (3, 3), activation='relu', input_shape=(1496, 257, 1)))
+    model.add(Conv2D(16, (3, 3), activation='relu', input_shape=(749, 129, 1)))
     model.add(Conv2D(16, (3, 3), activation='relu'))
     model.add(Flatten())
     model.add(Dense(128, activation='relu'))
@@ -89,24 +89,21 @@ def train_model(epoch_amt, save):
 
     if save:
         model.save("model")
+    return model
 
 
-def check_model(test):
+def check_model(model, test):
     successes = 0
     fails = 0
     for x in range(len(test)):
         X_test, y_test = test.as_numpy_iterator().next()
         yhat = model.predict(X_test)
         yhat = [1 if prediction > 0.5 else 0 for prediction in yhat]
-        if yhat[0] == y_test.astype(int)[0]:
-            successes += 1
-        else:
-            fails += 1
-        if yhat[1] == y_test.astype(int)[1]:
-            successes += 1
-        else:
-            fails += 1
-
+        for i in range(len(yhat)):
+            if yhat[i] == y_test.astype(int)[i]:
+                successes += 1
+            else:
+                fails += 1
     print(successes)
     print(fails)
 
@@ -117,27 +114,14 @@ if __name__ == "__main__":
     data = load_data()
     data = setup_data(data)
 
-    # filepath, label = positives.shuffle(buffer_size=10000).as_numpy_iterator().next()
-
-    # spectrogram, label = preprocess(filepath, label)
-
-    # plt.figure(figsize=(30, 20))
-    # plt.imshow(tf.transpose(spectrogram)[0])
-    # plt.show()
-
     training_size = int(len(data) * .80)        # 80% of data is for training
     testing_size = len(data) - training_size    # 20% of data is for validating
 
     train = data.take(training_size)
     test = data.skip(training_size).take(testing_size)
 
-    # samples, labels = train.as_numpy_iterator().next()
-    # print("-----------------------------------------------------")
-    # print(samples.shape)
-    # print("-----------------------------------------------------")
-
-    # train_model(10, False)
+    #model = train_model(8, False)
 
     model = load_model()
-    check_model(test)
+    check_model(model, test)
 
